36fd5885a9582ca6d2a6c3e8a0fe9c58628f766c
Mitchell <mitchellde@gmail.com>
2017-11-07 20:15:56 -0800

    Finally figured out lexical scoping, I think...

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates 4 sub-function all stored within the makeCacheMatrix environment, which is itself contained in the global environment.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtrx = matrix()) {
+  invrs <- NULL
+  # Create the 'set' sub-function
+  set <- function(y) {
+    mtrx <<- y
+    invrs <<- NULL
+  }
+  # Create the sub-function which retrieves the matrix
+  get <- function() mtrx
+  # Create the inverse making sub-function
+  setinverse <- function(inverse) invrs <<- inverse
+  # Finally, we create the sub-function that retrieves the inverse of the matrix
+  getinverse <- function() invrs
+  # And here's the list of sub-functions returned by the main function
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function accesses the makeCacheMatrix environment to solve for the matrix inverse, which is then stored in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get the inverse from the other function
+  invs <- x$getinverse()
+  # Check for cached data, let us know if it finds any, then gives it to us.
+  if(!is.null(invs)) {
+    message("Retrieving cached data")
+    return(invs)
+  }
+  # If there is no cached data, then we calculate the inverse here, cache it, and report it back.
+  matr <- x$get()
+  invs <- solve(matr, ...)
+  x$setinverse(invs)
+  invs
 }
